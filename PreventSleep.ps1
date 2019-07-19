40741114b09c202aa600db82b1686f85a288d38a
aditya-pathak <aditya-pathak@users.noreply.github.com>
2019-07-19 16:31:21 -0400

    Create PreventSleep.ps1
    Created PreventSleep script, helps to prevent windows from going to sleep mode and keeps Skype status as available.

---
+++ PreventSleep.ps1
@@ -0,0 +1,7 @@
+param($minutes = 600);
+$myshell = New-Object -com "Wscript.Shell";
+for ($i = 0; $i -lt $minutes; $i++) {
+	Start-Sleep -Seconds 60;
+	#$myshell.sendkeys(".");
+	$myshell.SendKeys("{SCROLLLOCK 2}");
+}
