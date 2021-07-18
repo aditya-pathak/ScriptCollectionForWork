f20204eba5f6ff7988e47e2d6f6e5d4878bb13ad
aditya-pathak <aditya-pathak@users.noreply.github.com>
2021-07-19 00:28:22 +0530

    Added new DNS types.

---
+++ 006 - OPENDNS/Set1.1.1.1DNS.bat
@@ -0,0 +1,2 @@
+netsh interface ip set dns "Wi-Fi" static 1.1.1.1
+netsh interface ip add dns "Wi-Fi" 1.0.0.1 index=2
---
+++ 006 - OPENDNS/SetGoogleDNS.bat
@@ -0,0 +1,2 @@
+netsh interface ip set dns "Wi-Fi" static 8.8.8.8
+netsh interface ip add dns "Wi-Fi" 8.8.4.4 index=2
